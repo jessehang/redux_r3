3861337c9252df370eeb51ee5c826553e528e900
Jesse Hang <hangtimee@gmail.com>
2019-06-28 17:43:22 -0700

    [oauth] initialized google oauth

---
+++ client/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import CLIENT_ID from './ClientId';
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: CLIENT_ID,
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
