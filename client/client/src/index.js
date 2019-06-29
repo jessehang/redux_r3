bc8864963325f42f8fcb963f89722dbebfb60a0b
Jesse Hang <hangtimee@gmail.com>
2019-06-28 21:23:39 -0700

    [redux] implemented authentication using redux

--- client/client/src/index.js
+++ client/client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-render(<App />, document.querySelector('#root'));
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
